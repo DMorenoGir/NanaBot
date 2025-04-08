834fed86b80f0157d0c4a2666e1a16bd62beab6c
Dave <davidmorex07@gmail.com>
2025-04-08 14:13:59 -0500

    Agrega mas palabras para desencadenar el flujo inicial

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -50,7 +50,7 @@ class MessageHandler {
   isGreeting(message) {
     const greetings = [
       "hola", "hello", "hi", "buenas", "buenos días", "buenas tardes", "buenas noches",
-      "buen día", "holi", "qué más", "saludos", "info", "informacion", "información", "más información", "mas informacion"
+      "buen día", "holi", "qué más", "saludos", "info", "informacion", "información", "más información", "mas informacion", "holaa"
     ];
     return greetings.some(greet => message.includes(greet));
   }
